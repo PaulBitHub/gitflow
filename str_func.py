b1d179edcee13a35bf39c4cb48ae313d93fd3465
PaulBitHub <p.bitiakov.official@gmail.com>
2024-05-27 08:32:03 +0300

    + новая функция + докстринг к предыдущей

--- str_func.py
+++ str_func.py
@@ -1,2 +1,13 @@
 def do_upper(data_string):
+    """Возвышает(!) все символы в верхний регистр в строке"""
     return data_string.upper()
+
+def do_upper_first_symbol(data_string):
+    """Делает первый символ в строке Заглавным"""
+    if not isinstance(data_string, str):
+        raise TypeError("Аргумент должен быть строкой.")
+
+    if not data_string:
+        return ""
+
+    return data_string[0].upper() + data_string[1:]
